99c34255cb798be5f96bfdc66cc60edbcd6aa929
omkarjadhav1011 <jadhavomkar101103@gmail.com>
2024-10-09 07:39:35 +0530

    Array Traval

--- Array/Inserstion.java
+++ Array/Inserstion.java
@@ -1,9 +1,17 @@
 public class Inserstion {
     public static void main(String args[]) {
-    
+      int arr[] = {1,2,3,5,6,7,9,0};
+
+      // function calling
+      insertElement(arr,7,10,4);
+
+      // Printing new array
+      for(int i=0 ; i<=arr.length;i++) {
+        System.out.println(arr[i]+" ");
+      }
     }
 
-    public void insertElement(int arr[],int n,int x,int pos) {
+    public static void insertElement(int arr[],int n,int x,int pos) {
         for(int i = n-1; i>=pos;i--) {
             arr[i+1] = arr[i];
         }
